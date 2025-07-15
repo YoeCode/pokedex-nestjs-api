d273f8d4583305e9b37af99a6bbb684f3985c758
YoeCode <y.marti@outlook.es>
2025-07-15 12:50:25 +0200

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { join } from 'path'; //Node
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+
+@Module({
+  imports: [ServeStaticModule.forRoot({
+rootPath: join(__dirname,'..','public'),renderPath: '/'
+}), PokemonModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
